eb0a253b403b52da0762cf473a3bf83f7c133757
TOUFANI ZOUHAIR <102550539+ImDeveloperz@users.noreply.github.com>
2025-04-21 19:07:35 +0100

    Update page.jsx

--- frontend/src/app/new-donations/page.jsx
+++ frontend/src/app/new-donations/page.jsx
@@ -5,7 +5,25 @@ import NewDonationPage from '../components/NewDonation'
 import useProtectedRoute from '../hooks/useProtectedRoute';
 
 const page = () => {
+  // Calling the custom hook for protected route access
   useProtectedRoute();
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Fetching the user from local storage
+    const storedUser = localStorage.getItem('user');
+    
+    if (storedUser) {
+      // Parsing the user data from JSON string
+      setUser(JSON.parse(storedUser).user);
+    }
+  }, []); // Empty array means this runs once when the component mounts
+
+  if (!user) {
+    // If user data is not available yet, return null or a loading spinner
+    return <BloodLoading />;
+  }
   return (
     <div>
         <HeaderAfterAuth/>
