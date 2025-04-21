145d9ead4a00e4211a0370f6ad33aa87209eabc7
zakariae-zemat <zakariaezemat@gmail.com>
2025-04-21 13:08:48 +0100

    simple changes

--- backend/auth.py
+++ backend/auth.py
@@ -12,7 +12,7 @@ class LoginRequest(BaseModel):
     email: str
     password: str
 
-@router.post("/login")
+@router.post("/log")
 def login(data: LoginRequest):
     users_ref = db.reference("users_hospital_bank")
     users = users_ref.get()
